b69af2100961714815a9d8076ba3e1f151b6405f
Octa Alief Kiemas Sahid <octa.aks@gmail.com>
2021-03-10 13:35:23 +0700

    init commit

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $admin = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@mail.com',
+            'password' => bcrypt('admin1234'),
+        ]);
+        $admin->assignRole('admin');
+    }
+}
