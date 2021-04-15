f2c8ad1a82b05de0c05f3c653cd27b791a95b5cc
joshuaht12 <yoscafisher12@gmail.com>
2021-04-15 18:05:45 +0700

    liturgi warta viewer fix

--- resources/views/frontend/view_liturgi.blade.php
+++ resources/views/frontend/view_liturgi.blade.php
@@ -1,36 +1,15 @@
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>GKE Haleluya Nanga Bulik | @yield('title')</title>
-
-    <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{ asset('admin-lte/plugins/fontawesome-free/css/all.min.css') }}">
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-    <!-- Tempusdominus Bootstrap 4 -->
-    <link rel="stylesheet"
-        href="{{ asset('admin-lte/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}">
-    <!-- iCheck -->
-    <link rel="stylesheet" href="{{ asset('admin-lte/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
-    <!-- JQVMap -->
-    <link rel="stylesheet" href="{{ asset('admin-lte/plugins/jqvmap/jqvmap.min.css') }}">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="{{ asset('admin-lte/dist/css/adminlte.min.css') }}">
-    <!-- overlayScrollbars -->
-    <link rel="stylesheet" href="{{ asset('admin-lte/plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
-    <!-- Daterange picker -->
-    <link rel="stylesheet" href="{{ asset('admin-lte/plugins/daterangepicker/daterangepicker.css') }}">
-    <!-- summernote -->
-    <link rel="stylesheet" href="{{ asset('admin-lte/plugins/summernote/summernote-bs4.min.css') }}">
-</head>
+@extends('layouts/frontend_master')
+@section('title', 'Liturgi')
+@section('content')
 
 <body>
-    <div class="container">
-        <iframe src="https://docs.google.com/viewer?url={{ asset('/') }}{{$liturgi->filename}}&embedded=true"
-            style="width:70%; height:80%;" frameborder="0"></iframe>
-            <embed src="{{ asset('/') }}{{$liturgi->filename}}" type="application/pdf" width="100%" height="600px" />
+    <div class="container ">
+        <div class="container ">
+            <iframe
+                src="https://drive.google.com/viewerng/viewer?url=https://{{ asset('/') }}{{$liturgi->filename}}?pid=explorer&efh=false&a=v&chrome=false&embedded=true"
+                width="100%" height="600" frameborder="0" scrolling="yes" />
+        </div>
     </div>
 </body>
+
+@endsection
--- resources/views/frontend/view_warta.blade.php
+++ resources/views/frontend/view_warta.blade.php
@@ -5,8 +5,11 @@
 <body>
     <div class="container ">
         <div class="container ">
-            <iframe src="https://docs.google.com/viewer?url={{$warta->filename}}&embedded=true"
-                style="width:70%; height:80%;" frameborder="0"></iframe>
+            <!-- <iframe src="https://docs.google.com/viewer?url={{$warta->filename}}&embedded=true"
+                style="width:70%; height:80%;" frameborder="0"></iframe> -->
+                <iframe
+                src="https://drive.google.com/viewerng/viewer?url=https://{{ asset('/') }}{{$warta->filename}}?pid=explorer&efh=false&a=v&chrome=false&embedded=true"
+                width="100%" height="600" frameborder="0" scrolling="yes" />
         </div>
     </div>
 </body>
