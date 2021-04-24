39b6ca770935bfa7400896635a29368d35a08d62
Octa Alief Kiemas Sahid <octa.aks@gmail.com>
2021-04-25 02:36:04 +0700

    footer stress

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="main-footer mt-5">
+<footer class="main-footer mt-5" id="footer">
     <!-- <div class="second-footer mt-3"> -->
     <!-- <img style="height:30px; margin-right:20px" src="/img/logo.png" alt=""> -->
     Copyright 2021 "©" Joeee. All Right
