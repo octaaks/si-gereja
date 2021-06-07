5cac5874220d07ce7c6afe2330ad636e57599831
Octa Alief Kiemas Sahid <octa.aks@gmail.com>
2021-06-07 17:48:32 +0700

    pdf viewer fix

--- resources/views/frontend/view_warta.blade.php
+++ resources/views/frontend/view_warta.blade.php
@@ -4,11 +4,13 @@
 
 <div class="container">
     <h4>{{$warta->title}}</h4>
-    <div class="d-flex justify-content-center">
-
+    <!-- <div class="d-flex justify-content-center">
         <iframe src="http://docs.google.com/gview?url={{ asset('/') }}{{$warta->filename}}&embedded=true" width="100%"
             height="600" frameborder="0" scrolling="yes"></iframe>
-
+    </div> -->
+    <div class="d-flex justify-content-center mt-5">
+        <embed src="{{ asset('/') }}{{$warta->filename}}#toolbar=1&navpanes=0&scrollbar=1" type="application/pdf"
+            width="100%" height="600px" />
     </div>
     <a class="btn btn-info mt-3" href="{{ asset('/') }}{{$warta->filename}}"><i class="fas fa-file-download"></i>
         DOWNLOAD
